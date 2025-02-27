770cc36b8de8a5f665e0619d3a58a56c14de926c
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2025-02-27 22:48:33 +0700

    fix: cookies

--- frontend/src/utils/cookies.js
+++ frontend/src/utils/cookies.js
@@ -2,15 +2,18 @@
 import { cookies } from "next/headers";
 
 export const setCookie = async (key, value, age) => {
-  cookies().set(key, value, {maxAge: age, httpOnly:true, secure:true})
+  const cookieStore = await cookies();
+  cookieStore.set(key, value, { maxAge: age, httpOnly: true, secure: true });
 };
 
 export const getCookie = async (key) => {
-  return cookies().get(key);
+  const cookieStore = await cookies();
+  return cookieStore.get(key);
 };
 
 export const removeCookie = async (...keys) => {
+  const cookieStore = await cookies();
   keys.forEach((key) => {
-    cookies().delete(key);
+    cookieStore.delete(key);
   });
 };
