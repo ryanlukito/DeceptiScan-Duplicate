567561fb29b326b8153ee9ed4b7db6859de3c64a
ryanlukito <ryanlukito13@gmail.com>
2025-03-03 09:28:14 +0700

    adding spam check and phising check pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -6,14 +7,24 @@ const Navbar = () => {
       <div className="flex items-center space-x-4">
         <div className="border-l-2 border-white pl-4">
           <h1 className="text-lg font-bold">DeceptiScan</h1>
-          <p className="text-sm">Spam and <br /> Phishing Detector</p>
+          <p className="text-sm">
+            Spam and <br /> Phishing Detector
+          </p>
         </div>
       </div>
       <div className="flex space-x-6 text-lg font-semibold">
-        <a href="/" className="hover:underline">Home</a>
-        <a href="#" className="hover:underline">Spam Check</a>
-        <a href="#" className="hover:underline">Phishing Check</a>
-        <a href="#" className="hover:underline">Articles</a>
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/SpamCheck" className="hover:underline">
+          Spam Check
+        </Link>
+        <Link href="/PhishingCheck" className="hover:underline">
+          Phishing Check
+        </Link>
+        <Link href="/Articles" className="hover:underline">
+          Articles
+        </Link>
       </div>
     </nav>
   );
