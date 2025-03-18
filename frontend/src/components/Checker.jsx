20b082b11a2381cdc3a27ef5ef06e39686df8cca
ryanlukito <ryanlukito13@gmail.com>
2025-03-18 13:17:13 +0700

    making checker response as a component

--- frontend/src/components/Checker.jsx
+++ frontend/src/components/Checker.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import CheckerResponse from "./CheckerResponse";
 
 const Checker = ({ predict_api, feedback_api, title, resultId }) => {
   const [text, setText] = useState("");
@@ -21,6 +22,7 @@ const Checker = ({ predict_api, feedback_api, title, resultId }) => {
 
     const data = await response.json();
     setResult(data);
+    // console.log(data.details[0][1]);
   };
 
   const handleFeedback = async (review) => {
@@ -62,32 +64,37 @@ const Checker = ({ predict_api, feedback_api, title, resultId }) => {
         </button>
       </form>
       {result && (
-        <div className="mt-4 p-3 bg-gray-200 rounded text-black">
-          <h3 className="font-bold">Prediction Result:</h3>
-          <p>{result.prediction}</p>
-          <h4 className="font-bold mt-2">Details:</h4>
-          <pre>{JSON.stringify(result.details, null, 2)}</pre>
-          <div className="flex space-x-4 mt-4">
-            {!feedbackSubmitted ? (
-              <>
-                <button
-                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 hover:transition ease-in cursor-pointer"
-                  onClick={() => handleFeedback("right")}
-                >
-                  I think the AI is right 👍
-                </button>
-                <button
-                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 hover:transition ease-in cursor-pointer"
-                  onClick={() => handleFeedback("wrong")}
-                >
-                  I think the AI is wrong 👎
-                </button>
-              </>
-            ) : (
-              <p>Thank you for your feedback</p>
-            )}
-          </div>
-        </div>
+        <CheckerResponse
+          result={result}
+          feedbackSubmitted={feedbackSubmitted}
+          handleFeedback={handleFeedback}
+        ></CheckerResponse>
+        // <div className="mt-4 p-3 bg-gray-200 rounded text-black">
+        //   <h3 className="font-bold">Prediction Result:</h3>
+        //   <p>{result.prediction}</p>
+        //   <h4 className="font-bold mt-2">Details:</h4>
+        //   <pre>{JSON.stringify(result.details, null, 2)}</pre>
+        //   <div className="flex space-x-4 mt-4">
+        //     {!feedbackSubmitted ? (
+        //       <>
+        //         <button
+        //           className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 hover:transition ease-in cursor-pointer"
+        //           onClick={() => handleFeedback("right")}
+        //         >
+        //           I think the AI is right 👍
+        //         </button>
+        //         <button
+        //           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 hover:transition ease-in cursor-pointer"
+        //           onClick={() => handleFeedback("wrong")}
+        //         >
+        //           I think the AI is wrong 👎
+        //         </button>
+        //       </>
+        //     ) : (
+        //       <p>Thank you for your feedback</p>
+        //     )}
+        //   </div>
+        // </div>
       )}
     </div>
   );
---
+++ frontend/src/components/CheckerResponse.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const CheckerResponse = ({ result, feedbackSubmitted, handleFeedback }) => {
+  return (
+    <div className="w-[55.885vw] h-[12.812vw] flex flex-col items-start justify-center text-black mt-[1vw]">
+      <div className="w-full h-[6.094vw] border border-black rounded-[0.26vw] p-[1vw] shadow-lg">
+        <h1 className="text-[1.25vw] italic">
+          <h1>
+            Your message has been identified as
+            {result.details[0][1] > 0.5
+              ? `${parseFloat((result.details[0][1] * 100).toFixed(2))}% ${
+                  result.prediction
+                }`
+              : ` ${parseFloat((result.details[0][0] * 100).toFixed(2))}% ${
+                  result.prediction
+                }`}
+            {`\u00A0`}
+            based on our AI Analysis
+          </h1>
+        </h1>
+      </div>
+      <h1 className="text-[1.25vw] mt-[0.5vw]">
+        Do you think that our DeceptiScan AI gave you the correct answer?
+      </h1>
+      <div className="flex space-x-4 mt-4">
+        {!feedbackSubmitted ? (
+          <>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 hover:transition ease-in cursor-pointer"
+              onClick={() => handleFeedback("right")}
+            >
+              I think the AI is right 👍
+            </button>
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 hover:transition ease-in cursor-pointer"
+              onClick={() => handleFeedback("wrong")}
+            >
+              I think the AI is wrong 👎
+            </button>
+          </>
+        ) : (
+          <p>Thank you for your feedback</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CheckerResponse;
